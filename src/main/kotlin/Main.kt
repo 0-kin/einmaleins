447678bdad038b6d9032fff3d8f5a132ab058863
0-kin <no@mail.com>
2023-03-28 20:04:18 +0200

    Reihe kann eingegeben werden

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,23 +1,57 @@
 fun main(args: Array<String>) {
-    println("Willkommen zur Einmaleins Challenge Niko")
+    println("Willkommen zur Einmaleins Challenge by Niko")
 
-    for(a in 1..10){
+    println("Welche Reihe möchtest du haben?")
+
+    val reihe = readLine()!!.toInt()
+
+    // for(reihe in 1..10){
         var b = 1
         while (b < 11) {
             try {
-                println("$b * $a =")
-                val ergebnis = a * b
+                println("$b * $reihe =")
+                val ergebnis = reihe * b
                 val eingabe = readLine()!!.toInt()
                 if (ergebnis == eingabe) {
-                    println("Richtig gerechnet du Genie")
+                    println("Richtig gerechnet du Genie!")
                     b = b + 1
                 } else {
-                    println("Falsch gerechnet du Dummkopf")
+                    println("Falsch gerechnet. Probier es nochmal:")
                 }
             } catch (e: Exception) {
                 println("Gib eine gültige Zahl ein")
             }
         }
-        println("------------------------------------------------")
-    }
+        println("Du bist super!!")
+        println("           ___\n" +
+                "     _..--\"\\  `|`\"\"--.._\n" +
+                "  .-'       \\  |        `'-.\n" +
+                " /           \\_|___...----'`\\\n" +
+                "|__,,..--\"\"``(_)--..__      |\n" +
+                "'\\     _.--'`.I._     ''--..'\n" +
+                "  `''\"`,#JGS/_|_\\###,---'`\n" +
+                "    ,#'  _.:`___`:-._ '#,\n" +
+                "   #'  ,~'-;(oIo);-'~, '#\n" +
+                "   #   `~-(  |    )=~`  #\n" +
+                "   #       | |_  |      #\n" +
+                "   #       ; ._. ;      #\n" +
+                "   #  _..-;|\\ - /|;-._  #\n" +
+                "   #-'   /_ \\\\_// _\\  '-#\n" +
+                " /`#    ; /__\\-'__\\;    #`\\\n" +
+                ";  #\\.--|  |O  O   |'-./#  ;\n" +
+                "|__#/   \\ _;O__O___/   \\#__|\n" +
+                " | #\\    [I_[_]__I]    /# |\n" +
+                " \\_(#   /  |O  O   \\   #)_/\n" +
+                "       /   |        \\\n" +
+                "      /    |         \\\n" +
+                "     /    /\\          \\\n" +
+                "    /     | `\\         ;\n" +
+                "   ;      \\   '.       |\n" +
+                "    \\-._.__\\     \\_..-'/\n" +
+                "     '.\\  \\-.._.-/  /'`\n" +
+                "        \\_.\\    /._/\n" +
+                "         \\_.;  ;._/\n" +
+                "       .-'-./  \\.-'-.\n" +
+                "      (___.'    '.___)")
+   // }
 }
