e92880f678516c28fa0cd9d01b699ef5e0cc734d
0-kin <no@mail.com>
2023-04-27 12:56:29 +0200

    Zufallsrechnungen Secherreihe

--- src/main/kotlin/six-challenge.kt
+++ src/main/kotlin/six-challenge.kt
@@ -6,13 +6,19 @@ fun main(){
 
     var punkte = 0
     for (i in 1..10){
-        println("$i * 6 = ")
-        val input = readLine()!!.toInt()
-        val result = i * 6
 
-        if (input == result){
+        val zahl = (1 .. 10).random()
+
+        println("$zahl * 6 = ")
+
+        var input = readLine()!!.toInt()
+
+        var ergebnis = zahl * 6
+
+        if(input == ergebnis) {
             punkte = punkte + 1
         }
+
     }
 
     println("Deine Punkte sind: $punkte")
